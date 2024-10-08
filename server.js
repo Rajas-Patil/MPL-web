458774d16fdda48b19823ad4bd832349fd7f6f8c
Rajas Patil <rajaspatil05@gmail.com>
2024-10-08 10:18:35 +0530

    modified js files

--- server.js
+++ server.js
@@ -3,18 +3,17 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const cors = require('cors');
 
+app.use(cors()); 
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 
-// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', orderRoutes);
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
